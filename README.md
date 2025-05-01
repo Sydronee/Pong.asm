e949fb860270eca2252aa050491e15d57c814b36
Sydronee <pizzaisgoodforhealth@gmail.com>
2025-05-01 14:29:02 +0530

    Initial Commit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pong.asm
-This is my attempt at Pong game in Assembly 8086
--- build.bat
+++
@@ -1,7 +0,0 @@
-masm Pong.asm;
-link Pong.obj
-@REM Commented out to not recompile previously complied code.
-@REM nasm Keyboard.asm -o Keyboard.obj -f obj 
-@REM tcc -c -IC:\TURBOC3\INCLUDE -LC:\TURBOC3\LIB Pong1.c
-tlink Pong1.obj Pong.obj Keyboard.obj, Pong.exe, Pong.map, C:\TURBOC3\LIB\C0S.OBJ C:\TURBOC3\LIB\CS.LIB
-Pong.exe
